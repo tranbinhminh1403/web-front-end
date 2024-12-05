b7e2e5a358a51c9a1f3313c5ec2f01b59d1e3e44
John Doe <johndoe@email.com>
2024-12-05 18:52:44 +0700

    fix home not showing product

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import Footer from "./Footer";
 import { Link } from "react-router-dom";
 import { formatCash } from "../utils/formatCash";
 import { ProductsCard } from "./ProductsCard";
+import { LoadingOutlined } from "@ant-design/icons";
 
 function Home() {
   const [isHovered, setIsHovered] = useState(false);
@@ -33,7 +34,7 @@ function Home() {
   }, []);
 
   useEffect(() => {
-    fetch(`http://localhost:8080/api/v1/products/filter?category=laptop`)
+    fetch(`https://web-back-end-1.onrender.com/api/v1/products/filter?category=laptop`)
       .then((response) => response.json())
       .then((data) => {
         if (data.success) {
@@ -48,7 +49,7 @@ function Home() {
   }, []);
 
   useEffect(() => {
-    fetch(`http://localhost:8080/api/v1/products/filter?category=pc`)
+    fetch(`https://web-back-end-1.onrender.com/api/v1/products/filter?category=pc`)
       .then((response) => response.json())
       .then((data) => {
         if (data.success) {
@@ -63,7 +64,7 @@ function Home() {
   }, []);
 
   useEffect(() => {
-    fetch(`http://localhost:8080/api/v1/products/filter?category=vga`)
+    fetch(`https://web-back-end-1.onrender.com/api/v1/products/filter?category=vga`)
       .then((response) => response.json())
       .then((data) => {
         if (data.success) {
@@ -212,6 +213,7 @@ function Home() {
               </div>
 
               
+              {products.length > 0 ? (
               <div className={`grid grid-cols-5 lg:gap-3 gap-1`}>
                 {products.slice(0, 5).map((product) => (
                   <Link
@@ -227,6 +229,11 @@ function Home() {
                   </Link>
                 ))}
               </div>
+              ) : (
+                <div className="flex justify-center items-center h-screen">
+                  <LoadingOutlined style={{ color: "#DC2626" }} />
+                </div>
+              )}
             </div>
           </div>
           <div className=" mt-8">
@@ -239,6 +246,8 @@ function Home() {
                 <div className="flex space-x-4"></div>
               </div>
 
+             
+              {productsPC.length > 0 ? (
               <div className={`grid grid-cols-5 lg:gap-3 gap-1`}>
                 {products.slice(0, 5).map((product) => (
                   <Link
@@ -252,8 +261,13 @@ function Home() {
                       <ProductsCard product={product} />
                     </div>
                   </Link>
-                ))}
-              </div>
+                  ))}
+                </div>
+              ) : (
+                <div className="flex justify-center items-center h-screen">
+                  <LoadingOutlined style={{ color: "#DC2626" }} />
+                </div>
+              )}
             </div>
           </div>
           <div className=" mt-8 mb-12">
@@ -267,6 +281,8 @@ function Home() {
                 <div className="flex space-x-4"></div>
               </div>
 
+              
+              {productsVGA.length > 0 ? (
               <div className={`grid grid-cols-5 lg:gap-3 gap-1`}>
                 {products.slice(0, 5).map((product) => (
                   <Link
@@ -282,6 +298,11 @@ function Home() {
                   </Link>
                 ))}
               </div>
+              ) : (
+                <div className="flex justify-center items-center h-screen">
+                  <LoadingOutlined style={{ color: "#DC2626" }} />
+                </div>
+              )}
             </div>
           </div>
         </div>
